4e970ebb4f9eb1554a565a7a700047258d9e8dc9
Franciane Pires <franpp22@gmail.com>
2023-08-10 17:10:17 -0300

    doc home: aligning tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,19 +31,19 @@ export default async function Home() {
               <a href={mainArticle.url} className="hover:text-blue-500 text-3xl">Saber mais</a>
             </div>
           </div>
-            <div className="w-full md:w-1/3 pl-3">
-              <h3 className="text-4xl font-semibold mb-2">Outras </h3>
-              <ul className="space-y-2">
-                {otherArticles.map((article: Article, index: number) => (
-                  <li key={index} className="bg-gray-200 hover:bg-red-800 rounded flex flex-col p-2">
-                    <a href="#" >{article.title}</a>
-                    <a href={article.url} className="hover:text-blue-500">Saber mais</a>
-                  </li>
-                ))}
-              </ul>
-            </div>
+          <div className="w-full md:w-1/3 pl-3">
+            <h3 className="text-4xl font-semibold mb-2">Outras </h3>
+            <ul className="space-y-2">
+              {otherArticles.map((article: Article, index: number) => (
+                <li key={index} className="bg-gray-200 hover:bg-red-800 rounded flex flex-col p-2">
+                  <a href="#" >{article.title}</a>
+                  <a href={article.url} className="hover:text-blue-500">Saber mais</a>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
+      </div>
     </div>
   );
 }
