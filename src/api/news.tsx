c65aeb4c21d6b572cb426db283250fbe24ff5e9d
Franciane Pires <franpp22@gmail.com>
2023-08-11 16:09:16 -0300

    doc api: creat

---
+++ src/api/news.tsx
@@ -0,0 +1,36 @@
+'use client' 
+import { useEffect, useState } from 'react';
+import { Root } from '@/interface/interfaceNews'; // Certifique-se de que o caminho para a interface está correto
+
+export default function NewsApi() {
+  // const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+  const [dataNews, setDataNews] = useState<Root | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const responseNews = await fetch(
+          `https://newsapi.org/v2/top-headlines?sources=google-news-br&apiKey=8c18442db94a48f5a1149d114f75ddef`
+        );
+
+        
+        console.log('1',responseNews)
+        if (!responseNews.ok) {
+          throw new Error('Erro ao buscar notícias');
+        }
+
+        console.log('2',responseNews)
+
+        const data: Root = await responseNews.json();
+        console.log('2',responseNews)
+        setDataNews(data);
+      } catch (error) {
+        console.error('Erro ao buscar notícias:', error);
+        setDataNews(null);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return dataNews;
+}
