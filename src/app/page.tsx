ba2a903cadb94b4a3e2516c66f4e5f978398aad3
Franciane Pires <franpp22@gmail.com>
2023-08-18 13:27:46 -0300

    switching API for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import NewsApi from '@/api/news';
-import { Article } from '@/interface/interfaceNews';
+import { Result } from '@/interface/interfaceNews';
 import Image from 'next/image';
 import React from 'react';
 
@@ -11,8 +11,8 @@ export default function Home() {
     return <p>Carregando...</p>;
   }
   
-  const mainArticle = dataNews.articles[0];
-  const otherArticles = dataNews.articles.slice(1);
+  const mainArticle = dataNews.results;
+  // const otherArticles = dataNews.results.slice(1);
 
   return (
     <div className="bg-gray-100">
@@ -25,15 +25,15 @@ export default function Home() {
             <h3 className="text-4xl font-semibold mb-2">Principal </h3>
             <Image src="" alt="Imagem da Notícia" className="w-full h-auto" />
             <div className="absolute inset-0 bg-opacity-50 flex flex-col justify-center p-6 text-white">
-              <h2 className="text-5xl font-bold mb-2">{mainArticle.title}</h2>
-              <p>{mainArticle.description}</p>
-              <a href={mainArticle.url}  target='_blank' className="hover:text-blue-500 text-3xl">Saber mais</a>
+              {/* <h2 className="text-5xl font-bold mb-2">{mainArticle.title}</h2> */}
+              {/* <p>{mainArticle.description}</p> */}
+              {/* <a href={mainArticle.url}  target='_blank' className="hover:text-blue-500 text-3xl">Saber mais</a> */}
             </div>
           </div>
           <div className="w-full md:w-1/3 pl-3">
             <h3 className="text-4xl font-semibold mb-2">Outras </h3>
             <ul className="space-y-2">
-              {otherArticles.map((article: Article, index: number) => (
+              {dataNews.results.map((article: Result, index: number) => (
                 <li key={index} className="bg-gray-700 hover:bg-red-800 rounded flex flex-col p-2 ">
                   <a href="#" >{article.title}</a>
                   <a href={article.url} target='_blank' className="hover:text-blue-500">Saber mais</a>
--- src/interface/interfaceNews.tsx
+++ src/interface/interfaceNews.tsx
@@ -1,21 +1,47 @@
 export interface Root {
-  status: string;
-  totalResults: number;
-  articles: Article[];
+  status: string
+  copyright: string
+  num_results: number
+  results: Result[]
 }
 
-export interface Article {
-  source: Source;
-  author: string;
-  title: string;
-  description: string; 
-  url: string;
-  urlToImage: string; 
-  publishedAt: string;
-  content: string; 
+export interface Result {
+  uri: string
+  url: string
+  id: number
+  asset_id: number
+  source: string
+  published_date: string
+  updated: string
+  section: string
+  subsection: string
+  nytdsection: string
+  adx_keywords: string
+  column: any
+  byline: string
+  type: string
+  title: string
+  abstract: string
+  des_facet: string[]
+  org_facet: string[]
+  per_facet: string[]
+  geo_facet: string[]
+  media: Medum[]
+  eta_id: number
 }
 
-export interface Source {
-  id: string;
-  name: string;
+export interface Medum {
+  type: string
+  subtype: string
+  caption: string
+  copyright: string
+  approved_for_syndication: number
+  "media-metadata": Metadaum[]
+}
+
+export interface Metadaum {
+  url: string
+  format: string
+  height: number
+  width: number
 }
